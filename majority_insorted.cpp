6aa85a14aa1a3e982a23f8918d53853b421f121f
swat <swramesh92@gmail.com>
2015-11-18 17:00:10 -0500

    Added b_search-buggy still

--- majority_insorted.cpp
+++ majority_insorted.cpp
@@ -16,8 +16,32 @@ void majority_linear( int X[], int n, int x_num)
 		}
 	}
 	cout<<"\n\t Not a Majority element!";
+}
 
-
+void majority_binary( int A[], int x, int a_num)
+{
+	/* as we need at least n/2 occurences, the condition is that the previous element is not x.
+	 * the other edge case can be when it is the first element and hence has no preceeding element.
+	 */
+	int pos=-1;
+	int beg=0, end=a_num-1, mid;
+	while(beg<=end&& end>0)
+	{
+//		cout<<"\n\t\t"<<mid<<endl;
+		mid=(beg+end)/2;
+		if(A[mid]==x)
+		{
+	//		cout<<"\n\t Finding Match at "<<mid<<endl;
+			if( (mid==0 && A[(a_num)/2+1]==x)||( A[mid-1]!=x && ( mid+a_num/2+1 < a_num && A[mid+a_num/2] == x)))
+			{ cout<<"\n YEs"; return;}
+//			else cout<<"\n Hello"<<"\t\t Prev elmnt "<<A[mid-1]<<" after n/2 elmnts"<<A[mid+a_num/2]<<endl;
+			end=mid-1;
+		}
+		else if( A[mid] <x)
+			beg=mid+1;
+		else end=mid-1;
+	}
+	cout<<"\n No";
 }
 main()
 {
@@ -27,6 +51,9 @@ main()
 	b_num=sizeof(B)/sizeof(int);
 	majority_linear(A,2, a_num);
 	majority_linear(B,2, b_num);
+	cout<<"\n bnary"<<endl;
+	majority_binary(A,2,a_num);
+	majority_binary(B,2,b_num);
 }
 
 
